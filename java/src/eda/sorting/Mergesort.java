9122cb75bd239566b525fc9c80a7e91ef8d8db21
Antonio López Marín <tonilopezmr@gmail.com>
2016-04-12 09:45:09 +0200

    Moved Mergesort to correct folder

---
+++ java/src/eda/sorting/Mergesort.java
@@ -0,0 +1,52 @@
+
+package eda.sorting;
+
+/**
+ * @author jose.llorens.ripolles@gmail.com
+ */
+public class Mergesort {
+
+    public static <T extends Comparable<T>> void mergesort(T[] ar) {
+        mergesort(ar, 0, ar.length - 1);
+    }
+
+    public static <T extends Comparable<T>> void mergesort(T[] ar, int i, int f) {
+        while (i < f) {
+            int m = (i + f) / 2;
+            mergesort(ar, i, m);
+            mergesort(ar, m + 1, f);
+            merge(ar, i, f, m);
+        }
+    }
+
+    public static <T extends Comparable<T>> void merge(T[] ar, int i, int f, int m) {
+        T[] aux = (T[]) new Comparable[f - i + 1];
+        int k = 0, a = i, b = m + 1;
+        while (a <= m || b <= f) {
+            if (ar[a].compareTo(ar[b]) < 1) {
+                aux[k] = ar[a];
+                k++;
+                a++;
+            } else {
+                aux[k] = ar[b];
+                k++;
+                b++;
+            }
+        }
+        while (a <= m) {
+            aux[k] = ar[a];
+            k++;
+            a++;
+        }
+        while (b <= f) {
+            aux[k] = ar[b];
+            k++;
+            b++;
+        }
+        for (k = 0; k < aux.length; k++) {
+            ar[i] = aux[k];
+
+        }
+    }
+
+}
