3b2fa4ddb71a66e9b3a73d991434cffc4e66575a
jose.llorens.ripolles@gmail.com <jose.llorens.ripolles@gmail.com>
2016-04-18 17:48:27 +0200

    Added JoseLlorensImp, my implementation od EDA exam problem

---
+++ java/src/problems/problem2/JoseLlorensImp.java
@@ -0,0 +1,20 @@
+package problems.problem2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class JoseLlorensImp {
+    public int numberOfMarks(int[] marks, int mark){
+        return numberOfMarks(marks,0,marks.length-1,mark);
+    }
+    public int numberOfMarks(int[]marks , int i, int j, int mark){
+        if(i>j) return 0 ;
+        int half = (i+j)/2;
+        if(marks[half]>mark){
+            return (j-half+1)+numberOfMarks(marks,i,half-1,mark);
+        }else{
+            return numberOfMarks(marks,half+1,j,mark);
+        }
+
+    }
+}
