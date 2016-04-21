12a27fb9d8306d3d080d8a0f77eebf6cb4d5eff4
Kevin Zarzycki Szymanski <kezarszy@inf.upv.es>
2016-04-21 17:20:07 +0200

    Resolved KevinImp

--- java/src/problems/problem2/KevinImp.java
+++ java/src/problems/problem2/KevinImp.java
@@ -10,13 +10,13 @@ public class KevinImp implements ExamMarks{
         if(ini == fin){
             if(marks[ini]>mark) return 1;
             else return 0;
-        } else if(ini > fin){
+        } else if(ini < fin){
             int mitad = (ini+fin)/2;
             if(marks[mitad] <= mark)
                 return numberOfMarks(marks, mitad+1, fin, mark);
             else
-                return numberOfMarks(marks, ini, mitad, mark) + numberOfMarks(marks, mitad+1, fin, mark);
-        }
-        return -1;
+                return 1 + numberOfMarks(marks, ini, mitad-1, mark) + numberOfMarks(marks, mitad+1, fin, mark);
+        }else
+            return 0;
     }
 }
