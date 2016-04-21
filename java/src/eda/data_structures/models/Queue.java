c5b94471bd077263d616f299e70b776d51ba31ab
jose.llorens.ripolles@gmail.com <jose.llorens.ripolles@gmail.com>
2016-04-21 16:49:02 +0200

    Initializes queue interface and ArrayQueue class

--- java/src/eda/data_structures/models/Queue.java
+++ java/src/eda/data_structures/models/Queue.java
@@ -3,5 +3,17 @@ package eda.data_structures.models;
 /**
  * Created by JoseLlorens on 20/04/2016.
  */
-public interface Queue {
+public interface Queue<T> {
+
+    void add(T x);
+
+    T element();
+
+    boolean offer(T x);
+
+    T peek();
+
+    T poll();
+
+    T remove();
 }
---
+++ java/src/eda/data_structures/queues/ArrayQueue.java
@@ -0,0 +1,57 @@
+package eda.data_structures.queues;
+
+import eda.data_structures.models.Queue;
+
+/**
+ * Created by JoseLlorens on 20/04/2016.
+ */
+public class ArrayQueue<T> implements Queue<T>{
+
+    T[] array ;
+    int head;
+    int tail;
+    int elements;
+    final int PREDEFYNIED_INITIAL_LENGTH = 50;
+
+    public ArrayQueue (){
+        array = (T[]) new Object[PREDEFYNIED_INITIAL_LENGTH];
+        head = 0;
+        tail = 0;
+    }
+    public ArrayQueue (int length){
+        array = (T[]) new Object[length];
+        head = 0;
+        tail = 0;
+    }
+
+    @Override
+    public void add(T x) {
+        array[tail]=x;
+        tail ++;
+    }
+
+    @Override
+    public T element() {
+        return null;
+    }
+
+    @Override
+    public boolean offer(T x) {
+        return false;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public T poll() {
+        return null;
+    }
+
+    @Override
+    public T remove() {
+        return null;
+    }
+}
