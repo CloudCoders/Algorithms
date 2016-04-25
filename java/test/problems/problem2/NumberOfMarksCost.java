eda84ebb719a0c99f4c6cf8bd8e3f1e2c97547a3
carmocca <carlossmocholi@hotmail.com>
2016-04-25 12:16:24 +0200

    Update from upstream

--- java/test/problems/problem2/NumberOfMarksCost.java
+++ java/test/problems/problem2/NumberOfMarksCost.java
@@ -17,11 +17,6 @@ public class NumberOfMarksCost {
     private static int SIZE_FIN = 1000000;
     private static int SIZE_INCR = 100000;
 
-<<<<<<< HEAD
-=======
-    //Mark to be beaten
-    //private static final int MARK = 50;
->>>>>>> fec085412c476dd1260895867d207559d25e25b5
     //Maximum mark possible
     private static final int MAX_ARRAY = 1000;
 
@@ -30,23 +25,14 @@ public class NumberOfMarksCost {
 
     public static void main(String args[]) {
 
-<<<<<<< HEAD
         //Array with each implementation of numberOfMarks
         List<ExamMarks> list = new ExamMarksFactory().create();
-=======
-        System.out.println("#-----------------------------------------");
-        System.out.println("#       Measurement of search times: ");
-        System.out.println("#        Marks from 0 to "+MAX_ARRAY+"    ");
-        System.out.println("#----------------------------------------- ");
-        System.out.println("#  Size    Search time                     ");
->>>>>>> fec085412c476dd1260895867d207559d25e25b5
 
         //Array that contains different random marks for each size
         List<List<Integer>> randomNumList = new ArrayList<>();
         //List of arrays to be generated randomly
         List<List<int[]>> randomArrayList = new ArrayList<>();
 
-<<<<<<< HEAD
         //Fill both lists
         for (int t = SIZE_INI, u = 0; t <= SIZE_FIN; t += SIZE_INCR, u++) {
             randomArrayList.add(new ArrayList<>());
@@ -54,29 +40,6 @@ public class NumberOfMarksCost {
             for (int v = 0; v < N; v++) {
                 randomArrayList.get(u).add(v, generateIncreasingRandoms(t, MAX_ARRAY));
                 randomNumList.get(u).add(v, new Random().nextInt(MAX_ARRAY + 1));
-=======
-        for (ExamMarks ex : list){
-            String clazz = ex.getClass().getName();
-            t1 = t2 = time = 0;
-            System.out.println("#--------------------------------------------------");
-            System.out.println("#  \t\t" + clazz.replaceFirst("problems.problem2.", "") + " implementation.");
-            System.out.println("#--------------------------------------------------");
-            for (int t = SIZE_INI; t <= SIZE_FIN; t += SIZE_INCR) {
-                //The mark should be random and not predefined
-                int mark = (int)(Math.random()*(MAX_ARRAY - 1));
-                vector = generateIncreasingRandoms(t, MAX_ARRAY);
-                t1 = System.nanoTime();
-                int n = ex.numberOfMarks(vector, mark);
-                t2 = System.nanoTime();
-                time += t2 - t1;
-                //The boolean correct check if the algorithm work correctly
-                boolean correct = (vector[vector.length-n-1]<=mark && vector[vector.length-n]>mark);
-                System.out.printf(Locale.US, " %1$8d %2$8.0f μs      %3$8d %4$8b\n",
-                        t,
-                        time / 1e3,
-                        n,
-                        correct);
->>>>>>> fec085412c476dd1260895867d207559d25e25b5
             }
         }
 
@@ -87,30 +50,31 @@ public class NumberOfMarksCost {
         System.out.println("#---------------------------------- ");
         System.out.println("#   Size       Search time");
 
-		for (int temp = 0; i < 2; i++){
-			for (ExamMarks ex : list){
+        for (int temp = 0; i < 2; i++){
+            for (ExamMarks ex : list){
 
-				String clazz = ex.getClass().getName();
-				double t1, t2, time;
+                String clazz = ex.getClass().getName();
+                double t1, t2, time;
 
-				System.out.println("#----------------------------------");
-				System.out.println("#  \t" + clazz.replaceFirst("problems.problem2.", "") + " implementation.");
-				System.out.println("#----------------------------------");
+                System.out.println("#----------------------------------");
+                System.out.println("#  \t" + clazz.replaceFirst("problems.problem2.", "") + " implementation.");
+                System.out.println("#----------------------------------");
 
-				for(int i = 0; i <randomArrayList.size(); i++){
-				    t1 = System.nanoTime();
-				    for(int j = 0; j < randomArrayList.get(i).size(); j++){
-				        ex.numberOfMarks(randomArrayList.get(i).get(j), randomNumList.get(i).get(j));
-				    }
-				    t2 = System.nanoTime();
-				    time = t2 - t1;
-				    System.out.printf(Locale.US, " %1$8d     %2$8.3f μs%n",
-				            randomArrayList.get(i).get(0).length,
-				            time / N / 1e3);
-				}
-			}
-		}
+                for(int i = 0; i <randomArrayList.size(); i++){
+                    t1 = System.nanoTime();
+                    for(int j = 0; j < randomArrayList.get(i).size(); j++){
+                        ex.numberOfMarks(randomArrayList.get(i).get(j), randomNumList.get(i).get(j));
+                    }
+                    t2 = System.nanoTime();
+                    time = t2 - t1;
+                    System.out.printf(Locale.US, " %1$8d     %2$8.3f μs%n",
+                            randomArrayList.get(i).get(0).length,
+                            time / N / 1e3);
+                }
+            }
+        }
     }
+    
     /**
      * @param amount "size of the array"
      * @param max "maximum possible number"
